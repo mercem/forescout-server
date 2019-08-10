8a8193deb65e75d7cb7e28b96461a692004898eb
Omer Cem Akyol <omercema@gmail.com>
2019-08-10 17:38:58 +0300

    first commit

---
+++ app/app.ts
@@ -0,0 +1,38 @@
+import express = require('express');
+
+let posts = require('../data/posts.json');
+
+const app: express.Application = express();
+const port = 3000
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.send('Forescout Blog API'));
+
+app.get('/posts', (req, res) => res.send(posts));
+
+app.get('/posts/:id', (req, res) => {
+    if(!posts[req.params.id]) res.status(404).send('Incorrect ID or post has been deleted already.');
+    else res.send(posts[req.params.id])
+  }
+);
+
+app.post('/posts', (req, res) => {
+  const {title, datePosted, categories, content} = req.body;
+  if(title && datePosted && categories && categories.length !== 0 && content){
+    posts.push({title, datePosted, categories, content});
+    res.send('Successfully added.');
+  } 
+  else res.status(400).send('Incorrect post data'); 
+})
+
+app.delete('/posts/:id', (req, res) => {
+  if(!posts[req.params.id]) res.status(404).send('Incorrect ID or post has been deleted already.');
+  else {
+    posts.splice(req.params.id, 1);
+    res.send('Successfully deleted.');
+  }
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
---
+++ build/app.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var posts = require('../data/posts.json');
+var app = express();
+var port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.get('/', function (req, res) { return res.send('Forescout Blog API'); });
+app.get('/posts', function (req, res) { return res.send(posts); });
+app.get('/posts/:id', function (req, res) {
+    if (!posts[req.params.id])
+        res.status(404).send('Incorrect ID or post has been deleted.');
+    else
+        res.send(posts[req.params.id]);
+});
+app.post('/posts', function (req, res) {
+    var _a = req.body, title = _a.title, datePosted = _a.datePosted, categories = _a.categories, content = _a.content;
+    if (title && datePosted && categories && categories.length !== 0 && content) {
+        posts.push({ title: title, datePosted: datePosted, categories: categories, content: content });
+        res.send('Successfully added.');
+    }
+    else
+        res.status(400).send('Incorrect post data');
+});
+app.delete('/posts/:id', function (req, res) {
+    if (!posts[req.params.id])
+        res.status(404).send('Incorrect ID or post has been deleted.');
+    else {
+        posts.splice(req.params.id, 1);
+        res.send('Successfully deleted.');
+    }
+});
+app.listen(port, function () { return console.log("Listening on port " + port + "!"); });
